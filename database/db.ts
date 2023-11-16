726366611aed60ff7d5af88ea24b973e14c15cc5
sergio <sergio.sejo@gmail.com>
2023-11-16 21:46:05 +0100

    added console to process.env

--- database/db.ts
+++ database/db.ts
@@ -27,6 +27,7 @@ export const connect = async() => {
 
         await mongoose.disconnect();
     }
+    console.log('process.env: ', process.env);
     await mongoose.connect( process.env.MONGO_URL || '');
     mongoConnection.isConnected = 1;
     console.log('Connected to MongoDB:', process.env.MONGO_URL );

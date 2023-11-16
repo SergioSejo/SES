18621eaba093c641ec4fbb0f2b8a0308834f64e4
sergio <sergio.sejo@gmail.com>
2023-11-16 16:27:39 +0100

    add info in layout

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -13,6 +13,8 @@ export const Layout:FC<PropsWithChildren<Props>> = ({ title = 'SES', children })
       <Box sx={{ height:'100%', flexFlow: 1 }}>
         <Head>
             <title>{ title }</title>
+            <meta name="author" content="Sergio Jiménez Yébenes" />
+            <meta name="keywords" content={ `Sad eyes, soccer, match, statistic, matches, statistics`} />
         </Head>
 
         <Navbar />

4df1aed1f439f4c945c53fd32ee234e4e75c25cd
sergio <sergio.sejo@gmail.com>
2023-11-20 18:50:42 +0100

    try with vercel

--- database/db.ts
+++ database/db.ts
@@ -28,7 +28,7 @@ export const connect = async() => {
         await mongoose.disconnect();
     }
     console.log('process.env: ', process.env);
-    await mongoose.connect( process.env.MONGO_URL || '');
+    await mongoose.connect( 'mongodb://' + process.env.VERCEL_URL + '/ses' || '');
     mongoConnection.isConnected = 1;
     console.log('Connected to MongoDB:', process.env.MONGO_URL );
 }
